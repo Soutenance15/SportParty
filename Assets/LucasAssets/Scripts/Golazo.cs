3b71e8e1b67096d3c9c817e0ca38dbd7252c2a8e
NinjYab <lucasclain097@gmail.com>
2025-10-20 14:22:44 +0400

    intégration scene main

--- Assets/LucasAssets/Scripts/Golazo.cs
+++ Assets/LucasAssets/Scripts/Golazo.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Golazo : MonoBehaviour
@@ -11,6 +13,10 @@ public class Golazo : MonoBehaviour
 
     public int p1Score;
     public int p2Score;
+    public float endDelay = 1f;
+
+    private string player1Name;
+    private string player2Name;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,15 +31,45 @@ public class Golazo : MonoBehaviour
     public void P1Score()
     {
         p1Score++;
-        player1FootScore.text = "P1:" + p1Score.ToString();
+        player1FootScore.text = $"{GameDataManager.Player1}:" + p1Score.ToString();
         Destroy(GameObject.FindGameObjectWithTag("Ball"));
     }
-    
-     public void P2Score()
+
+    public void P2Score()
     {
         p2Score++;
-        player2FootScore.text = "P2:" + p2Score.ToString();
+        player2FootScore.text = $"{GameDataManager.Player2}" + p2Score.ToString();
         Destroy(GameObject.FindGameObjectWithTag("Ball"));
+    }
+
+    private void EndGame(string winner, string loser, bool isPlayer1Winner)
+    {
+        if (scorePanel.activeSelf) return;
+
+        string p1 = GameDataManager.Player1;
+        string p2 = GameDataManager.Player2;
+
+        if (isPlayer1Winner)
+        {
+            winText.text = $"{GameDataManager.Player1} Wins!";
+            GameDataManager.AddChampPoints(p1, 25);
+        }
+        else
+        {
+            GameDataManager.AddChampPoints(p2, 25);
+            winText.text = $"{GameDataManager.Player2} Wins!";
+        }
+
+        Time.timeScale = 0;
+        scorePanel.SetActive(true);
+
+        StartCoroutine(EndSequence());
+    }
+    
+    IEnumerator EndSequence()
+    {
+                yield return new WaitForSeconds(endDelay);
+        SceneManager.LoadScene("MiniGameSelector");
     }
 
     // Update is called once per frame
@@ -41,17 +77,11 @@ public class Golazo : MonoBehaviour
     {
         if (p1Score >= 5)
         {
-            player1FootScore.text = "P1 WINS!";
-            scorePanel.SetActive(true);
-            winText.text = "Player 1 Wins!";
-
-
+          EndGame(winner: player1Name, loser: player2Name, isPlayer1Winner: true);
         }
         if (p2Score >= 5)
         {
-            player2FootScore.text = "P2 WINS!";
-            scorePanel.SetActive(true);
-            winText.text = "Player 2 Wins!";
+           EndGame(winner: player2Name, loser: player1Name, isPlayer1Winner: false);
         }
     }
 }
